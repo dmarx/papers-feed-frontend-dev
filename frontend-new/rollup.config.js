a2d5d5e2a6bfa5b7615fb85bdf2001cfe1808f3f
David Marx <david.marx84@gmail.com>
2025-03-20 20:12:58 -0700

    Update rollup.config.js

--- frontend-new/rollup.config.js
+++ frontend-new/rollup.config.js
@@ -19,6 +19,14 @@ export default {
     sourcemap: !production
   },
   plugins: [
+    // This plugin helps to strip 'use client' directives
+    {
+      name: 'strip-use-client',
+      transform(code) {
+        // Remove 'use client' directive
+        return code.replace(/['"]use client['"];?\n?/g, '');
+      }
+    },
     replace({
       'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development'),
       preventAssignment: true
